01986511e575798a6a78df23adda7c9039bd967b
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2024-09-08 21:01:52 +0200

    fix:
      - remove unncessary preCacheImage

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "63dabbac64fdaccf1900d234766d17f5",
+const RESOURCES = {"flutter_bootstrap.js": "a31a3437448b9ba854cfe06ea09da2b1",
 "version.json": "1e29bcb188654863d4cd11c1abda5adf",
-"index.html": "be67d1f490d64224d379a1a38f44133b",
-"/": "be67d1f490d64224d379a1a38f44133b",
-"main.dart.js": "847f3d53495f8195861f3140133cebaf",
+"index.html": "9759142be3ef29f52982887359bf37c8",
+"/": "9759142be3ef29f52982887359bf37c8",
+"main.dart.js": "d05ee6e39c854ac310ac2eb6ca567bd0",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "e1eecab042e10d1e328b707b89f24695",
 "icons/Icon-192.png": "518337d0cfdfac64dd371051680a0c06",
